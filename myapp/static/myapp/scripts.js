bf7c3c91ef820102c71d6f9e278ba051c6ed0201
Ayush <ayushkushwaha321@gmail.com>
2023-11-11 17:08:57 +0530

    added charts and preloader

--- myapp/static/myapp/scripts.js
+++ myapp/static/myapp/scripts.js
@@ -2,17 +2,12 @@ $(document).ready(function () {
     let data;
     // Toggle sidebar
     $("#toggleFiltersBtn").click(function () {
-        //     $("#toggleFiltersBtn").text(function (index, text) {
-        //         return text.trim() === "Show Filters" ? "X" : "Show Filters";
-        //     });
-
-        // Toggle the hamburger icon and cross icon
         $(".hamburger-icon").toggleClass("hidden");
         $(".cross-icon").toggleClass("hidden");
 
 
         $(".sidebar").toggleClass("show");
-        $(".main-content").toggleClass("expanded");
+        $(".container-fluid").toggleClass("expanded-container");
         $("#toggleFiltersBtn").toggleClass("expanded");
     });
 
@@ -39,7 +34,6 @@ $(document).ready(function () {
         showData(filteredData)
     });
 
-
     $('#resetFiltersBtn').on('click', function () {
         $('#endYearFilter, #startYearFilter, #topicsFilter, #sectorFilter, #regionFilter, #pestFilter, #sourceFilter, #countryFilter').val('all');
         showData(data)
@@ -60,6 +54,10 @@ $(document).ready(function () {
         },
     });
 
+    function sortArrayByX(arr) {
+        return arr.sort((a, b) => a.x.localeCompare(b.x));
+    }
+
     function showData(data) {
         var intensityData = data.reduce((acc, item) => {
             var existingItem = acc.find((entry) => entry.x === item.sector);
@@ -70,20 +68,156 @@ $(document).ready(function () {
             }
             return acc;
         }, []);
+        intensityData = sortArrayByX(intensityData)
 
-        // Create a bar chart
-        createBarChart(
+        var likelihoodDataBySector = data.reduce((acc, item) => {
+            var existingItem = acc.find((entry) => entry.x === item.sector);
+            if (existingItem) {
+                existingItem.y += item.likelihood;
+            } else {
+                acc.push({ x: item.sector, y: item.likelihood });
+            }
+            return acc;
+        }, []);
+        likelihoodDataBySector = sortArrayByX(likelihoodDataBySector)
+
+        var relevanceDataBySector = data.reduce((acc, item) => {
+            var existingItem = acc.find((entry) => entry.x === item.sector);
+            if (existingItem) {
+                existingItem.y += item.relevance;
+            } else {
+                acc.push({ x: item.sector, y: item.relevance });
+            }
+            return acc;
+        }, []);
+        relevanceDataBySector = sortArrayByX(relevanceDataBySector)
+
+        var intensityDataOverTime = data.reduce((acc, item) => {
+            // var dateObject = new Date(item.published);
+            // var year = dateObject.getFullYear();
+            // console.log(year)
+            var existingItem = acc.find((entry) => entry.x === item.start_year);
+            if (existingItem) {
+                existingItem.y += item.intensity;
+            } else {
+                acc.push({ x: item.start_year, y: item.intensity });
+            }
+            return acc;
+        }, []);
+        intensityDataOverTime = sortArrayByX(intensityDataOverTime)
+
+        var sectorCountByRegion = data.reduce((acc, item) => {
+            // Assuming you have a property called 'country' in your data
+            var country = item.region;
+
+            // Check if the country is already in the accumulator
+            var existingCountry = acc.find((entry) => entry.x === country);
+
+            if (existingCountry) {
+                // Increment the sector count for the existing country
+                existingCountry.y++;
+            } else {
+                // Add a new entry for the country with a count of 1
+                acc.push({ x: country, y: 1 });
+            }
+
+            return acc;
+        }, []);
+        sectorCountByRegion = sortArrayByX(sectorCountByRegion)
+
+        var metricsByCountry = data.reduce((acc, item) => {
+            // Assuming you have a property called 'country' in your data
+            var country = item.country;
+
+            // Check if the country is already in the accumulator
+            var existingCountry = acc.find((entry) => entry.x === country);
+
+            if (existingCountry) {
+                // Sum the metrics for the existing country
+                existingCountry.likelihood += item.likelihood || 0;
+                existingCountry.relevance += item.relevance || 0;
+                existingCountry.intensity += item.intensity || 0;
+            } else {
+                // Add a new entry for the country with the metrics
+                acc.push({
+                    x: country,
+                    likelihood: item.likelihood || 0,
+                    relevance: item.relevance || 0,
+                    intensity: item.intensity || 0,
+                });
+            }
+
+            return acc;
+        }, []);
+        metricsByCountry = sortArrayByX(metricsByCountry)
+
+        var dataForTopics = data.reduce((acc, item) => {
+            // Assuming you have a property called 'country' in your data
+            var country = item.topic;
+
+            // Check if the country is already in the accumulator
+            var existingCountry = acc.find((entry) => entry.x === country);
+
+            if (existingCountry) {
+                // Sum the metrics for the existing country
+                existingCountry.likelihood += item.likelihood || 0;
+                existingCountry.relevance += item.relevance || 0;
+                existingCountry.intensity += item.intensity || 0;
+            } else {
+                // Add a new entry for the country with the metrics
+                acc.push({
+                    x: country,
+                    likelihood: item.likelihood || 0,
+                    relevance: item.relevance || 0,
+                    intensity: item.intensity || 0,
+                });
+            }
+
+            return acc;
+        }, []);
+        dataForTopics = sortArrayByX(dataForTopics)
+
+        createAreaChart(
             "intensityChart",
-            "Intensity Distribution",
+            "Intensity Distribution By Sector",
             "Intensity",
-            intensityData
+            intensityData,
         );
+
         createBarChart(
-            "likelihoodChart",
-            "Likelihood Distribution",
-            "Intensity",
-            intensityData
+            "likelihoodChartSector",
+            "Likelihood By Sector",
+            "Likelihood",
+            likelihoodDataBySector
         );
+
+        createBarChart(
+            "relevanceChartBySector",
+            "Relevance By Sector",
+            "Relevance",
+            relevanceDataBySector
+        );
+
+        createAreaChart(
+            "intensityOverStartTime",
+            "Intensity Over Start Time",
+            "Intensity",
+            intensityDataOverTime,
+            false
+        );
+
+        createPieChart(
+            "regionDistribution",
+            "Count Of Sector Over Region",
+            "Intensity",
+            sectorCountByRegion,
+        );
+
+        createRadarChart('metricsByCountry', 'Metrics by Country', 'Metrics', metricsByCountry);
+
+        createMultiLineChart('multiLineChart', 'Topics Distribution', dataForTopics);
+
+        document.getElementById('preloader').style.display = 'none';
 
     }
 
@@ -95,18 +229,18 @@ $(document).ready(function () {
             // Destroy the existing chart instance
             ctx.chart.destroy();
         }
-
+        var colors = generateRandomColors(data.length);
         // Create a new chart instance
         ctx.chart = new Chart(ctx.getContext("2d"), {
             type: "bar",
             data: {
-                labels: data.map((item) => (item.x != "" ? item.x : "No Sector")),
+                labels: data.map((item) => (item.x != "" ? item.x : "(Blank)")),
                 datasets: [
                     {
                         label: label,
                         data: data.map((item) => item.y),
-                        backgroundColor: "rgba(75, 192, 192, 0.2)",
-                        borderColor: "rgba(75, 192, 192, 1)",
+                        backgroundColor: colors,
+                        borderColor: colors.map((color) => color.replace("0.2", "1")),
                         borderWidth: 1,
                     },
                 ],
@@ -137,6 +271,220 @@ $(document).ready(function () {
         });
     }
 
+    function createPieChart(chartId, chartTitle, label, data, chartType = 'doughnut') {
+        var ctx = document.getElementById(chartId);
+
+        // Check if a chart instance already exists
+        if (ctx.chart) {
+            // Destroy the existing chart instance
+            ctx.chart.destroy();
+        }
+
+        // Create a new chart instance
+        ctx.chart = new Chart(ctx.getContext("2d"), {
+            type: chartType,
+            data: {
+                labels: data.map((item) => (item.x != "" ? item.x : "(Blank)")),
+                datasets: [
+                    {
+                        label: label,
+                        data: data.map((item) => item.y),
+                        borderWidth: 1,
+                    },
+                ],
+            },
+            options: {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                return `${tooltipItem.label}: ${tooltipItem.formattedValue}`;
+                            },
+                            footer: function (tooltipItems) {
+                                var filters = getAppliedFilters(); // Assuming you have a function getAppliedFilters() to get applied filters
+                                return filters.join("\n");
+                            },
+                        },
+                    },
+                },
+            },
+        });
+    }
+
+    function createAreaChart(chartId, chartTitle, label, data, isFill = true) {
+        var ctx = document.getElementById(chartId);
+
+        // Check if a chart instance already exists
+        if (ctx.chart) {
+            // Destroy the existing chart instance
+            ctx.chart.destroy();
+        }
+
+        var colors = generateRandomColors(data.length);
+        // Create a new chart instance
+        ctx.chart = new Chart(ctx.getContext("2d"), {
+            type: "line", // Use "line" type for area chart
+            data: {
+                labels: data.map((item) => (item.x != "" ? item.x : "(Blank)")),
+                datasets: [
+                    {
+                        label: label,
+                        data: data.map((item) => item.y),
+                        backgroundColor: colors,
+                        borderColor: colors.map((color) => color.replace("0.2", "1")),
+                        borderWidth: 1,
+                        fill: isFill, // Fill the area under the line
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                return `${tooltipItem.label}: ${tooltipItem.formattedValue}`;
+                            },
+                            footer: function (tooltipItems) {
+                                var filters = getAppliedFilters(); // Assuming you have a function getAppliedFilters() to get applied filters
+                                return filters.join("\n");
+                            },
+                        },
+                    },
+                },
+            },
+        });
+    }
+
+    function createRadarChart(chartId, chartTitle, label, data) {
+        var ctx = document.getElementById(chartId);
+
+        // Check if a chart instance already exists
+        if (ctx.chart) {
+            // Destroy the existing chart instance
+            ctx.chart.destroy();
+        }
+
+        // Extract labels (countries) and metrics from the data
+        var labels = data.map((item) => (item.x != "" ? item.x : "(Blank)"));
+        var likelihoodData = data.map((item) => item.likelihood);
+        var relevanceData = data.map((item) => item.relevance);
+        var intensityData = data.map((item) => item.intensity);
+
+        // Create a new chart instance
+        ctx.chart = new Chart(ctx.getContext("2d"), {
+            type: "radar",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Likelihood",
+                        data: likelihoodData,
+                        backgroundColor: "rgba(75, 192, 192, 0.2)",
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1,
+                        pointBackgroundColor: "rgba(75, 192, 192, 1)",
+                        pointBorderColor: "#fff",
+                        pointHoverBackgroundColor: "#fff",
+                        pointHoverBorderColor: "rgba(75, 192, 192, 1)",
+                    },
+                    {
+                        label: "Relevance",
+                        data: relevanceData,
+                        backgroundColor: "rgba(255, 99, 132, 0.2)", // Adjust color as needed
+                        borderColor: "rgba(255, 99, 132, 1)", // Adjust color as needed
+                        borderWidth: 1,
+                        pointBackgroundColor: "rgba(255, 99, 132, 1)", // Adjust color as needed
+                        pointBorderColor: "#fff",
+                        pointHoverBackgroundColor: "#fff",
+                        pointHoverBorderColor: "rgba(255, 99, 132, 1)", // Adjust color as needed
+                    },
+                    {
+                        label: "Intensity",
+                        data: intensityData,
+                        backgroundColor: "rgba(255, 205, 86, 0.2)", // Adjust color as needed
+                        borderColor: "rgba(255, 205, 86, 1)", // Adjust color as needed
+                        borderWidth: 1,
+                        pointBackgroundColor: "rgba(255, 205, 86, 1)", // Adjust color as needed
+                        pointBorderColor: "#fff",
+                        pointHoverBackgroundColor: "#fff",
+                        pointHoverBorderColor: "rgba(255, 205, 86, 1)", // Adjust color as needed
+                    },
+                ],
+            },
+            options: {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                return `${tooltipItem.dataset.label}: ${tooltipItem.formattedValue}`;
+                            },
+                            footer: function (tooltipItems) {
+                                var filters = getAppliedFilters(); // Assuming you have a function getAppliedFilters() to get applied filters
+                                return filters.join("\n");
+                            },
+                        },
+                    },
+                },
+            },
+        });
+    }
+
+    function createMultiLineChart(chartId, chartTitle, data) {
+        var ctx = document.getElementById(chartId);
+
+        // Check if a chart instance already exists
+        if (ctx.chart) {
+            // Destroy the existing chart instance
+            ctx.chart.destroy();
+        }
+
+        // Extract labels (topics) and metrics from the data
+        var labels = data.map((item) => item.x);
+
+        // Extract unique metrics (likelihood, relevance, intensity) from the data
+        var uniqueMetrics = Object.keys(data[0]).filter((key) => key !== 'x');
+
+        // Create datasets for each metric
+        var datasets = uniqueMetrics.map((metric) => {
+            return {
+                label: metric.charAt(0).toUpperCase() + metric.slice(1), // Capitalize the first letter
+                data: data.map((item) => item[metric]),
+                // borderColor: generateRandomColors(), // Function to get a random color
+                borderWidth: 1,
+                fill: false,
+            };
+        });
+
+        // Create a new chart instance
+        ctx.chart = new Chart(ctx.getContext("2d"), {
+            type: "line",
+            data: {
+                labels: labels,
+                datasets: datasets,
+            },
+            options: {
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                return `${tooltipItem.dataset.label}: ${tooltipItem.formattedValue}`;
+                            },
+                            footer: function (tooltipItems) {
+                                var filters = getAppliedFilters(); // Assuming you have a function getAppliedFilters() to get applied filters
+                                return filters.join("\n");
+                            },
+                        },
+                    },
+                },
+            },
+        });
+    }
+
     function getAppliedFilters() {
         var appliedFilters = [];
         var filterIds = [
@@ -159,5 +507,19 @@ $(document).ready(function () {
 
         return appliedFilters;
     }
+
+    function generateRandomColors(count) {
+        var colors = [];
+        for (var i = 0; i < count; i++) {
+            var color = "rgba(" +
+                Math.floor(Math.random() * 256) + "," +
+                Math.floor(Math.random() * 256) + "," +
+                Math.floor(Math.random() * 256) + ",0.2)";
+            colors.push(color);
+        }
+        return colors;
+    }
+
+
 
 });
